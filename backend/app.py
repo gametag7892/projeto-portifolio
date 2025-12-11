105dc64d09bf3d495bad7632b7cc95fcdca15e3a
gametag7892 <eduardosoaresmota3@gmail.com>
2025-12-10 23:52:39 -0300

    Modificação do backend

--- backend/app.py
+++ backend/app.py
@@ -1,6 +1,15 @@
-from flask import Flask
-
+from flask import Flask, jsonify
+from flask_cors import CORS
 app = Flask(__name__)
+
+CORS(app)
+
+@app.route('/api/mensagem', method=['GET'])
+def get_mensagem():
+    return jsonify({
+        'sucesso': True,
+        'mensagem': 'Dados do Flask recebidos com sucesso!'
+    })
 
 @app.route("/")
 def hello_world():
