811c4e2ce0279e243ff37e75178bb69f3a68c405
gametag7892 <eduardosoaresmota3@gmail.com>
2025-12-13 18:37:53 -0300

    Versão 2.0 GPT

--- my-app/src/app/page.js
+++ my-app/src/app/page.js
@@ -5,7 +5,6 @@ import React, { useState } from 'react';
 function NavBar() {
   return (
     <div className="header">
-
       <div className="nav-bar">
         <ul className="list-nav">
           <li className="item"><a href="#">Inicio</a></li>
@@ -14,18 +13,11 @@ function NavBar() {
           <li className="item"><a href="https://github.com/gametag7892">GitHub</a></li>
         </ul>
       </div>
-
-      <div className="button">
-        <a href="#">Login</a>
-      </div>
-
-      <div className="button">
-        <a href="#">Create</a>
-      </div>
-
     </div>
   );
 }
+
+
 
 function PopUp(){
   const [mostrarModal, setMostrarModal] = useState(true);
@@ -55,29 +47,60 @@ function PopUp(){
 function Title() {
   return (
     <div className="Title">
-      <h1>SEJA BEM VINDO</h1>
+      <h2>Seja bem vindo ao meu ✨Projeto✨</h2>
     </div>
   );
 }
 
-function Carriagem(){
-  return(
-    <div>
-      <img></img>
-      <img></img>
-      <img></img>
-      <img></img>
+function Subtitle(){
+  return (
+    <div className="Subtitle">
+      <h3>Uma IA que responde perguntas sobre mim</h3>
     </div>
   );
 }
+
+function Top(){
+  return(
+    <div className="topbar">
+      <div className="top-left">
+        <h3 className="myNick"><a href="https://github.com/gametag7892">GameTag7892</a></h3>
+      </div>
+
+      <div className="top-center">
+      </div>
+      
+      <div className="top-right">
+        <button className="button">
+          <a href="#">Login</a>
+        </button>
+
+      </div>
+    </div>
+  );
+}
+
 
 export default function Home() {
   return (
     <>
-      <PopUp/>
-      <NavBar/>
-      <Title/>
-      <Carriagem/>
+      <main className="main">
+        <NavBar/>
+        <div className="board">
+          <Top/>
+          <div className="central">
+            <div>
+              <Title/>
+              <Subtitle/>
+            </div>
+            
+            <div className='inputBox'>
+              <input className="input" placeholder="Coloque sua dúvida sobre mim"></input>
+              <button className="button">Enviar</button>
+            </div>
+          </div>
+        </div>
+      </main>
     </>
   );
 }
